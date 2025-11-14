78a5b35c3b51e12fb8f4e83a2a128d62662fcfa5
NanthaKumar1710 <nkpharma17@gmail.com>
2025-11-14 10:17:34 +0530

    SECOND COMMIT

--- projectForJenkins/src/test/java/sample/EnterUrlTest.java
+++ projectForJenkins/src/test/java/sample/EnterUrlTest.java
@@ -6,6 +6,14 @@ public class EnterUrlTest {
 	@Test
 	public void navigateToApp() {
 		System.out.println("navigate app sucessfully via url");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	}
 
 }
--- projectForJenkins/src/test/java/sample/LoginTest.java
+++ projectForJenkins/src/test/java/sample/LoginTest.java
@@ -6,6 +6,9 @@ public class LoginTest {
 	@Test
 	public void loginToApp() {
 		System.out.println("login in sucessfully....");
+//		String BROWSER = System.getProperty("browser");
+//		System.out.println(BROWSER);
+
 	}
 
 }
